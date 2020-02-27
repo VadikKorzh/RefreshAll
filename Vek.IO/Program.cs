d136f7f6199a4c9899eab28011b61268a571d164
vadik.korzh <vadikkorzh@gmail.com>
2020-02-27 09:09:30 +0200

    fun with streams

--- Vek.IO/Program.cs
+++ Vek.IO/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace Vek.IO
 {
@@ -9,7 +11,88 @@ namespace Vek.IO
         static void Main(string[] args)
         {
             //GetDriveInfo();
-            DirectoryInfoForFun();
+            //DirectoryInfoForFun();
+            //DirectoryStaticFun();
+            //FileInfoForFun();
+            //StreamForFun();
+
+            EncodingForFun();
+        }
+
+        private static void StreamForFun()
+        {
+            using (var fStr = File.Open(@"D:\Worker\IO\DirectoryInfo\data.txt", FileMode.OpenOrCreate))
+            {
+                String text = "Team explorer home";
+                var bytes = Encoding.ASCII.GetBytes(text);
+                fStr.Write(bytes, 0, bytes.Length);
+
+                fStr.Position = 0;
+
+                List<Byte> bytesOut = new List<byte>();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    bytesOut.Add((Byte)fStr.ReadByte());
+                }
+
+                Console.WriteLine(Encoding.UTF32.GetString(bytesOut.ToArray()));
+            }
+        }
+
+        private static void EncodingForFun()
+        {
+            String testText = "Vadik Korzh";
+            foreach (var encoding in Encoding.GetEncodings())
+            {
+                Console.WriteLine(encoding.Name);
+                Console.WriteLine(encoding.DisplayName);
+                Console.WriteLine(encoding.CodePage);
+                var encodingInstance = encoding.GetEncoding();
+                foreach (var b in encodingInstance.GetBytes(testText))
+                {
+                    Console.Write(b + " ");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        private static void FileInfoForFun()
+        {
+            var fileNames = new[] { "file.txt", "file_1.cmd", "img.bmp" };
+            var dirPath = @"D:\Worker\IO\DirectoryInfo\SubFolder3\Folder1";
+
+            //foreach (var fn in fileNames)
+            //{
+            //    var fi1 = new FileInfo(Path.Combine(dirPath, fn));
+            //    fi1.Create();
+            //}
+
+            var fi = new FileInfo(Path.Combine(dirPath, fileNames[0]));
+            using (var fStr = fi.Open(FileMode.Append, FileAccess.Write, FileShare.Write))
+            {
+                using (var wrtr = new StreamWriter(fStr))
+                {
+                    //wrtr.NewLine = "*********";
+                    wrtr.WriteLine("wwwwww");
+                }
+            }
+
+            fi.Replace(Path.Combine(dirPath, fileNames[1]), Path.Combine(dirPath, fileNames[2]));
+            //ShowFileInfo(fi);
+        }
+
+        private static void DirectoryStaticFun()
+        {
+            var drives = Directory.GetLogicalDrives();
+
+            foreach (var drive in drives)
+            {
+                Console.WriteLine(drive);
+            }
+
+            Directory.SetCreationTime(@"D:\Worker\IO\DirectoryInfo\SubFolder2\f1 — копия (4).txt",
+                DateTime.Now.AddYears(100));
         }
 
         private static void DirectoryInfoForFun()
@@ -37,8 +120,9 @@ namespace Vek.IO
 
             var options = new EnumerationOptions
             {
-                 MatchType = MatchType.Simple,
-                 MatchCasing = MatchCasing.CaseSensitive, RecurseSubdirectories = true
+                MatchType = MatchType.Simple,
+                MatchCasing = MatchCasing.CaseSensitive,
+                RecurseSubdirectories = true
             };
 
             foreach (var dir in dirInfo.EnumerateDirectories("*Folder?", options))
